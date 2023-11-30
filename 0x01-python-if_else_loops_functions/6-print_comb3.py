ee3ac788888ac89360ce24a65d0fef9408b73733
Se7a4 <mohamed.sarhan123789@gmail.com>
2023-11-30 03:29:59 +0200

    comp3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for x in range(0, 8):
     for y in range(0, 10):
         if x != y and y > x:
-            print("{}{}".format(x , y), end=", ")
+            print("{}{}".format(x, y), end=", ")
 print("89")
