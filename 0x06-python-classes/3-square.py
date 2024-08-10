5482fc009cfc345f09db7fa9f3f2e6c83a8320b1
mohamed sarhan <mohamed.sarhan123789@gmail.com>
2024-08-10 19:32:30 +0300

    4th_class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class named Square"""
+
+class Square:
+    """
+    class that defines properties of square by: (based on 2-square.py)
+    Attributes:
+        size : size of a square.
+    """
+    def __init__(self, size=0):
+        """creats a new instance of square 
+        Args :
+            size : size of the square.
+        """
+        self.__size = size 
+        if not isinstance(size, int):
+            raise TypeError ("size must be an integer")
+        elif size < 0 :
+            raise ValueError ("size must be >= 0")
+        else :
+            self.__size = size
+    def area(self):
+        """public instance method 
+        Returns: The current square area.
+        """
+        return (self.__size*self.__size)
