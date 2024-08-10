d6d2c026932fd90b5b36a52f314c0385b1776d4f
mohamed sarhan <mohamed.sarhan123789@gmail.com>
2024-08-10 23:50:51 +0300

    print_method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -53,8 +53,8 @@ class Square:
         """
         if self.__size == 0:
             print()
-        for i in range (self.__size):
-            for j in range (self.__size):
+        for i in range(self.__size):
+            for j in range(self.__size):
                 print("#", end="")
-                j+=1
+                j += 1
             print("")
