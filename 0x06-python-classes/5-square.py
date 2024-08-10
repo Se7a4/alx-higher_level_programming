266a6d8124ee9f9988d8269ab04550f66ecee3f8
mohamed sarhan <mohamed.sarhan123789@gmail.com>
2024-08-10 23:29:01 +0300

    print_method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -51,7 +51,6 @@ class Square:
     def my_print(self):
         """prints in stdout the square with the character #
         """
-
         if self.__size == 0:
             print()
         for i in range (self.__size):
