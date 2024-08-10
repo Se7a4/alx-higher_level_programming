31ee50c1edfe5aa61233d7e23ef6b7630afde516
mohamed sarhan <mohamed.sarhan123789@gmail.com>
2024-08-10 18:34:17 +0300

    2nd_class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" define a class named square """
+
+
+class Square():
+    """ define a class attripute. """
+    __size = None
+    def __init__(self , size):
+        self.__size = size
