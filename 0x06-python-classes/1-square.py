24a6e94a3c9ddaf3d32e5558721d50375b899195
mohamed sarhan <mohamed.sarhan123789@gmail.com>
2024-08-10 18:42:27 +0300

    2nd_class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,11 +2,17 @@
 """ define a class named square """
 
 
-class Square():
-    """ creat new instance of square.
+class Square:
+    """ A class that defines the attributes of square 
+
+    Attributes:
+    size: size of the square
+    """
+    
+    def __init__(self , size):
+        """ creat new instance of square.
 
     Args: 
         size: size of the square
     """
-    def __init__(self , size):
         self.__size = size
